b8c1101b9a533b7024997fb9c746f590d5e9da14
Wilver Arana R <wilver.ar.dev@gmail.com>
2024-06-26 12:46:35 -0500

    feat: add - profile police

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -34,7 +33,9 @@ class _LoginState extends State<LoginAdmin> {
   final TextEditingController _controller2 = TextEditingController();
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
-  AuthService _authService = AuthService();
+  bool _isLoading = false;
+
+  final AuthService _authService = AuthService();
   String? _deviceToken;
 
   @override
@@ -58,122 +59,169 @@ class _LoginState extends State<LoginAdmin> {
 
   void _login() async {
     if (_formKey.currentState?.validate() ?? false) {
+      FocusScope.of(context).unfocus();
+
+      setState(() {
+        _isLoading = true;
+      });
+
       final username = _controller.text;
       final password = _controller2.text;
 
-      final response = await _authService.login(username, password, _deviceToken ?? 'no-device-token');
 
-      final Map<String, dynamic> tokenData = jsonDecode(response.body) as Map<String, dynamic>;
+      try {
+        final response = await _authService.login(username, password, _deviceToken ?? 'no-device-token');
+        print(response.body);
+        final Map<String, dynamic> tokenData = jsonDecode(response.body) as Map<String, dynamic>;
 
-      if (tokenData['token'] != null && tokenData['token'].toString().isNotEmpty) {
-        Provider.of<AuthProvider>(context, listen: false).login(tokenData['token'].toString());
-      } else {
-        // Show error message
-        showDialog(
-          context: context,
-          builder: (context) {
-            return AlertDialog(
-              title: const Text('Error'),
-              content: const Text('Usuario o contraseña incorrectos'),
-              actions: [
-                TextButton(
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                  child: const Text('Cerrar'),
-                ),
-              ],
-            );
-          },
-        );
+        if (mounted) {
+          setState(() {
+            _isLoading = false;
+          });
+
+          if (tokenData['token'] != null && tokenData['token'].toString().isNotEmpty) {
+            Provider.of<AuthProvider>(context, listen: false).login(tokenData['token']);
+          } else {
+            _showErrorDialog('Usuario o contraseña incorrectos');
+          }
+        }
+      } catch (e) {
+        if (mounted) {
+          setState(() {
+            _isLoading = false;
+          });
+          if (e is UnauthorizedException) {
+            _showErrorDialog('Ocurrió un error. Por favor, intente nuevamente.');
+          } else {
+            _showErrorDialog('Usuario o contraseña incorrectos');
+          }
+        }
       }
     }
   }
 
+  void _showErrorDialog(String message) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Error'),
+          content: Text(message),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Cerrar'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    final colors = Theme.of(context).colorScheme;
-
     return Scaffold(
-      body: Center(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 40.0),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const SizedBox(height: 20),
-                  Image.asset(
-                    'assets/logo.png',
-                    height: 100,
-                  ),
-                  const SizedBox(height: 20),
-                  const Text(
-                    'Bienvenido Policía',
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const SizedBox(height: 10),
-                  const Text(
-                    'Por favor, inicie sesión para continuar',
-                    style: TextStyle(fontSize: 16, color: Colors.grey),
-                    textAlign: TextAlign.center,
-                  ),
-                  const SizedBox(height: 40),
-                  UserInputField(
-                    controller: _controller,
-                    hint: 'Usuario',
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Por favor, ingrese su usuario';
-                      }
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: 20),
-                  UserInputField(
-                    controller: _controller2,
-                    hint: 'Contraseña',
-                    obscureText: true,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Por favor, ingrese su contraseña';
-                      }
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: 30),
-                  ElevatedButton.icon(
-                    onPressed: _login,
-                    label: const Text('Iniciar sesión'),
-                    icon: Icon(Icons.login),
-                    style: ElevatedButton.styleFrom(
-                      padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 12),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20),
+      body: Stack(
+        children: [
+          Center(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 40.0),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const SizedBox(height: 20),
+                      Image.asset(
+                        'assets/logo.png',
+                        height: 100,
                       ),
-                    ),
+                      const SizedBox(height: 20),
+                      const Text(
+                        'Bienvenido Ciudadano',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      const Text(
+                        'Por favor, inicie sesión para continuar',
+                        style: TextStyle(fontSize: 16, color: Colors.grey),
+                        textAlign: TextAlign.center,
+                      ),
+                      const SizedBox(height: 40),
+                      UserInputField(
+                        controller: _controller,
+                        hint: 'Usuario',
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Por favor, ingrese su usuario';
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(height: 20),
+                      UserInputField(
+                        controller: _controller2,
+                        hint: 'Contraseña',
+                        obscureText: true,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Por favor, ingrese su contraseña';
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(height: 30),
+                      ElevatedButton.icon(
+                        onPressed: _login,
+                        icon: const Icon(Icons.login),
+                        label: const Text('Iniciar sesión'),
+                        style: ElevatedButton.styleFrom(
+                          padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 12),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          TextButton(
+                            onPressed: () {
+                              print('...Ayuda...');
+                            },
+                            child: const Text(
+                              '¿Necesitas ayuda? Contacta a tu administrador',
+                              style: TextStyle(color: Colors.red),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
-                  const SizedBox(height: 20),
-                  TextButton(
-                    onPressed: () {
-                      print('Ayuda');
-                    },
-                    child: const Text(
-                      '¿Necesitas ayuda? Contacta a tu administrador',
-                      style: TextStyle(color: Colors.red),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                ],
+                ),
               ),
             ),
           ),
-        ),
+          if (_isLoading)
+            Stack(
+              children: [
+                ModalBarrier(
+                  color: Colors.black.withOpacity(0.3),
+                  dismissible: false,
+                ),
+                const Center(
+                  child: CircularProgressIndicator(),
+                ),
+              ],
+            ),
+        ],
       ),
     );
   }
